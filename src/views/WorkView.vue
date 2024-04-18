30c517b608702f24b3fa45d6ede8ccc317ad5c59
AEllegaard <annaeskytte@gmail.com>
2024-04-18 13:17:26 +0200

    nye routes til work

--- src/views/WorkView.vue
+++ src/views/WorkView.vue
@@ -23,7 +23,7 @@ export default {
   <div class="grid grid-cols-4 mx-4 gap-4 z-10 relative pb-20 mt-20">
 
     <div class="container col-start-1 mt-20 drop-shadow-3xl aspect-square bg-grå w-1/2">
-  <WorkExample link="/project/studentid" image="../src/components/media/CD-studiekort.png" class=""></WorkExample>
+  <WorkExample link="/project/studentid" image="/media/CD-studiekort.png" class=""></WorkExample>
   <div class="overlay flex justify-center items-center" style="pointer-events: none;">
     <p class="text-hvid opacity-100">Student ID</p>
   </div>
@@ -42,13 +42,13 @@ export default {
     </div>
 
     <div class="container bg-grå col-start-3 mt-10 ml-4 w-2/3 drop-shadow-3xl aspect-square">
-      <WorkExampleVideo link="/project/flowfields" video="src/components/media/flowfield.mov" class=""></WorkExampleVideo>
+      <WorkExampleVideo link="/project/flowfields" video="/media/flowfield.mov" class=""></WorkExampleVideo>
       <div class="overlay flex justify-center items-center" style="pointer-events: none;">
         <p class="text-hvid opacity-100">Flowfield</p></div>
     </div>
 
     <div class="container bg-grå col-start-1 mt-4 w-2/3 drop-shadow-3xl aspect-square grayscale">
-      <WorkExample link="/project/blender" image="src/components/media/Finalcloseup1ørender-kopi.png" class=""></WorkExample>
+      <WorkExample link="/project/blender" image="/media/Finalcloseup1ørender-kopi.png" class=""></WorkExample>
       <div class="overlay flex justify-center items-center" style="pointer-events: none;">
         <p class="text-hvid opacity-100">Blender</p></div>
     </div>
@@ -60,25 +60,25 @@ export default {
     </div>
 
     <div class="container bg-grå col-start-4 mt-32  drop-shadow-3xl aspect-square grayscale">
-      <WorkExampleVideo link="/project/imagesampling" video="src/components/media/sample.mov" class=""></WorkExampleVideo>
+      <WorkExampleVideo link="/project/imagesampling" video="/media/sample.mov" class=""></WorkExampleVideo>
       <div class="overlay flex justify-center items-center" style="pointer-events: none;">
         <p class="text-hvid opacity-100">Image Sampling</p></div>
     </div>
 
     <div class="container grayscale bg-grå col-start-2 w-3/4 aspect-square drop-shadow-3xl justify-self-end">
-      <WorkExample link="/project/kontrastfisk" image="src/components/media/kontrastfisk.png" class=""></WorkExample>
+      <WorkExample link="/project/kontrastfisk" image="/media/kontrastfisk.png" class=""></WorkExample>
       <div class="overlay flex justify-center items-center" style="pointer-events: none;">
         <p class="text-hvid opacity-100">Fish Poster</p></div>
     </div>
     
     <div class="container grayscale bg-grå col-start-4 mt-44 w-2/3 aspect-square drop-shadow-3xl">
-      <WorkExample link="/project/albumcover" image="src/components/media/teyeger.png" class=""></WorkExample>
+      <WorkExample link="/project/albumcover" image="/media/teyeger.png" class=""></WorkExample>
       <div class="overlay flex justify-center items-center" style="pointer-events: none;">
         <p class="text-hvid opacity-100">Album cover</p></div>
     </div>
 
     <div class="container grayscale bg-grå col-start-1 drop-shadow-3xl">
-      <WorkExampleVideo link="/project/karsten" video="src/components/media/karstendesk-kopi.mov" class=""></WorkExampleVideo>
+      <WorkExampleVideo link="/project/karsten" video="/media/karstendesk-kopi.mov" class=""></WorkExampleVideo>
       <div class="overlay flex justify-center items-center" style="pointer-events: none;">
         <p class="text-hvid opacity-100">Desktop</p></div>
     </div>
