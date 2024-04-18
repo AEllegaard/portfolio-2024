b84089b3849382dd2930765745dc4c6207fced87
AEllegaard <annaeskytte@gmail.com>
2024-04-18 13:42:49 +0200

    nye billeder

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,7 +4,7 @@
     <img class="bg-grå col-start-3  aspect-square w-3/4 drop-shadow-3xl bread" src="/media/IMG_8292.png" alt="">
     <p class="text-hvid font-thin text-sm  col-start-1 w-2/3 tracking-widest leading-relaxed">Did you know Lorem Ipsum has roots in classical music? <br> Hi I'm Anna Ellegaard a 24 yr old Creative coder with design on my mind.</p>
     <div class="mt-28">
-    <img class="grayscale bg-grå col-start-2 aspect-square w-3/4 drop-shadow-3xl object-top vin" src="../components/media/received_588660709922674.png" alt="">
+    <img class="grayscale bg-grå col-start-2 aspect-square w-3/4 drop-shadow-3xl object-top vin" src="/media/received_588660709922674.png" alt="">
      </div>
      <p class="text-hvid font-thin justify-self-end text-end mt-72 col-start-4 w-2/3 tracking-widest text-sm ">My current favorite Emoji: 🙂‍↔️ </p>
 
@@ -22,7 +22,7 @@
         <p class="text-xs mt-2 tracking-widest leading-normal">It's a two-year program blending graphic communication, UX design, programming, and digital marketing. With hands-on projects and industry-standard tools like Adobe Creative Suite, students create visually compelling designs for digital platforms, preparing for careers in digital media or further studies in digital design.</p>
      </div>
 
-     <img class="col-start-1 aspect-square w-3/4 drop-shadow-3xl grayscale berry" src="../components/media/1000005829.jpg" alt="">
+     <img class="col-start-1 aspect-square w-3/4 drop-shadow-3xl grayscale berry" src="/media/1000005829.jpg" alt="">
 
   
      <div class="grid grid-cols-4 col-start-1 col-span-4 mt-8">
